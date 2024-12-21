895c5f7a9fa77c084ce15a597211ab887b99d40a
Anvar <101347819+anvaraxadjonov1802@users.noreply.github.com>
2024-12-21 19:02:57 +0500

    Delete core directory

--- core/aaccaunt/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Victims
-
-@admin.register(Victims)
-class VictimsAdmin (admin.ModelAdmin):
-    list_display = ['id', 'username', 'password', 'created_at']
-    class Meta:
-        model = Victims
